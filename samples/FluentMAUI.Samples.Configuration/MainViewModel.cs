8894853de0eff058f9a98da5e06b784d6780f250
Lars Krämer <lars.kraemer@live.de>
2023-04-20 17:46:27 +0200

    FM-1: added sample ui

--- samples/FluentMAUI.Samples.Configuration/MainViewModel.cs
+++ samples/FluentMAUI.Samples.Configuration/MainViewModel.cs
@@ -1,14 +1,24 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Configuration;
 using System;
 
 namespace FluentMAUI.Samples.Configuration;
 
-public class MainViewModel : ObservableObject
+public partial class MainViewModel : ObservableObject
 {
+    private readonly IConfiguration _configuration;
     [ObservableProperty] string titleMessage = string.Empty;
 
-    public MainViewModel()
+    public MainViewModel(IConfiguration configuration)
 	{
-	}
-}
+        this._configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
 
+    [RelayCommand]
+    public async Task OnViewAppearingAsync(CancellationToken cancellationToken)
+    {
+        string title = this._configuration.GetSection("Title").Value;
+        this.TitleMessage = title;
+    }
+}
