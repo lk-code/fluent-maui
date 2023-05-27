63d4c3c3508924d8b380adbf497d0bbb4c37f1cb
Lars Krämer <lars.kraemer@live.de>
2023-05-27 15:34:21 +0200

    FM6: added ToggleButton with event Demo

--- source/FluentMAUI.UI/Controls/ToggleButton.cs
+++ source/FluentMAUI.UI/Controls/ToggleButton.cs
@@ -7,8 +7,10 @@ public class ToggleButton : Button, IToggleButton
 {
     private Color _primaryBackgroundColor = null;
     private Brush _primaryBackground = null;
+    private Color _primaryTextColor = null;
 
     private bool _isChecked = false;
+
     public bool IsChecked
     {
         get { return this._isChecked; }
@@ -48,9 +50,22 @@ public class ToggleButton : Button, IToggleButton
         set => SetValue(SelectedBackgroundProperty, value);
     }
 
+    public static readonly BindableProperty SelectedTextColorProperty = BindableProperty.Create(
+        "SelectedTextColor",
+        typeof(Color),
+        typeof(ToggleButton),
+        null);
+
+    public Color SelectedTextColor
+    {
+        get => (Color)GetValue(SelectedTextColorProperty);
+        set => SetValue(SelectedTextColorProperty, value);
+    }
+
     public ToggleButton() : base()
     {
         this.Clicked += ToggleButton_Clicked;
+        ApplyStandardButtonStyle();
     }
 
     ~ToggleButton()
@@ -60,22 +75,65 @@ public class ToggleButton : Button, IToggleButton
 
     private void ToggleButton_Clicked(object sender, System.EventArgs e)
     {
+        this.Toggle();
+    }
+
+    /// <inheritdoc />
+    public void Toggle()
+    {
         this.IsChecked = !this.IsChecked;
-        
+
         this.Checked?.Invoke(this, new CheckedEventArgs(this.IsChecked));
 
         if (this.IsChecked)
         {
             this._primaryBackgroundColor = this.BackgroundColor;
             this._primaryBackground = this.Background;
+            this._primaryTextColor = this.TextColor;
 
             this.BackgroundColor = this.SelectedBackgroundColor;
             this.Background = this.SelectedBackground;
+            this.TextColor = this.SelectedTextColor;
         }
         else
         {
             this.BackgroundColor = this._primaryBackgroundColor;
             this.Background = this._primaryBackground;
+            this.TextColor = this._primaryTextColor;
         }
+    }
+
+    private void ApplyStandardButtonStyle()
+    {
+        // copy style from Button
+        Button btn = new Button();
+
+        this.Background = btn.Background;
+        this.CornerRadius = btn.CornerRadius;
+        this.Margin = btn.Margin;
+        this.Opacity = btn.Opacity;
+        this.Padding = btn.Padding;
+        this.Resources = btn.Resources;
+        this.Rotation = btn.Rotation;
+        this.Scale = btn.Scale;
+        this.Shadow = btn.Shadow;
+        this.Style = btn.Style;
+        this.Visual = btn.Visual;
+        this.BorderColor = btn.BorderColor;
+        this.BorderWidth = btn.BorderWidth;
+        this.CharacterSpacing = btn.CharacterSpacing;
+        this.FlowDirection = btn.FlowDirection;
+        this.FontAttributes = btn.FontAttributes;
+        this.FontFamily = btn.FontFamily;
+        this.FontSize = btn.FontSize;
+        this.FontAutoScalingEnabled = this.FontAutoScalingEnabled;
+        this.HeightRequest = btn.HeightRequest;
+        this.WidthRequest = btn.WidthRequest;
+        this.MaximumHeightRequest = btn.MaximumHeightRequest;
+        this.MaximumWidthRequest = btn.MaximumWidthRequest;
+        this.MinimumHeightRequest = btn.MinimumHeightRequest;
+        this.MinimumWidthRequest = btn.MinimumWidthRequest;
+        this.TextColor = btn.TextColor;
+        this.TextTransform = btn.TextTransform;
     }
 }
--- source/FluentMAUI.UI/Interfaces/IToggleButton.cs
+++ source/FluentMAUI.UI/Interfaces/IToggleButton.cs
@@ -5,4 +5,9 @@ namespace FluentMAUI.UI.Interfaces;
 public interface IToggleButton
 {
     event EventHandler<CheckedEventArgs> Checked;
+    
+    /// <summary>
+    /// Toggles the state of the button.
+    /// </summary>
+    void Toggle();
 }
