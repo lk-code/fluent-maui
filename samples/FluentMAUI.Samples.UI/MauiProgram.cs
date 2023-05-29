5fc48fa0b5ad5b1608851247a8db47874f2ff080
Lars <lars.kraemer@live.de>
2023-05-29 23:38:26 +0200

    added winui acrylic demo

--- samples/FluentMAUI.Samples.UI/MauiProgram.cs
+++ samples/FluentMAUI.Samples.UI/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Maui;
 using FluentMAUI.UI;
 using FluentMAUI.UI.Controls;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace FluentMAUI.Samples.UI;
 
@@ -18,6 +19,21 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+        builder.ConfigureLifecycleEvents(events =>
+        {
+#if WINDOWS10_0_17763_0_OR_GREATER
+
+            events.AddWindows(wndLifeCycleBuilder =>
+            {
+                wndLifeCycleBuilder.OnWindowCreated(window =>
+                {
+                    // *** For Mica or Acrylic support ** //
+                    window.TryMicaOrAcrylic();
+                });
+            });
+#endif
+        });
+
 
 
         builder.UseFluentUi(options =>
@@ -29,21 +45,21 @@ public static class MauiProgram
 
         builder.Services.AddSingleton<MainPage>();
         builder.Services.AddSingleton<MainViewModel>();
-        
+
         builder.Services.AddSingleton<Controls.BasicInput>();
         builder.Services.AddSingleton<Controls.BasicInputVM>();
-        
+
         builder.Services.AddSingleton<Controls.ToggleButton.ToggleButtonWithEvent>();
         builder.Services.AddSingleton<Controls.ToggleButton.ToggleButtonWithEventVM>();
         builder.Services.AddSingleton<Controls.ToggleButton.ToggleButtonWithCommand>();
         builder.Services.AddSingleton<Controls.ToggleButton.ToggleButtonWithCommandVM>();
-        
+
         builder.Services.AddSingleton<Controls.Layout>();
         builder.Services.AddSingleton<Controls.LayoutVM>();
-        
+
         builder.Services.AddSingleton<Controls.Widget.WidgetsInsideGrid>();
         builder.Services.AddSingleton<Controls.Widget.WidgetsInsideGridVM>();
-        
+
         return builder.Build();
     }
 }
